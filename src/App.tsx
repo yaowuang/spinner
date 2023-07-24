1b8d1df78cd2f3dd3cb90b9bc87b984cbb8091e0
Yao Saechao <yaosaechao@yaos-mbp.lan>
2023-07-24 11:23:37 -0700

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Router, } from '@solidjs/router';
 import { Component, createSignal } from 'solid-js';
 import { ColorWheelPicker } from './components/ColorWheelPicker';
+import { inject } from '@vercel/analytics'
 
 const App: Component = () => {
-
-
+	inject();
 	const [pageTitle, setPageTitle] = createSignal("Random Color Wheel Picker")
 		return (
 			<Router>
